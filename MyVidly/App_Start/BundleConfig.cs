82bacdd91afc9f28d1ca959ca29f57884fc8886e
Ankit <ankit@siemens.com>
2018-08-29 21:00:42 +0530

    Add new theme

--- MyVidly/App_Start/BundleConfig.cs
+++ MyVidly/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MyVidly
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap - lumen.css",
                       "~/Content/site.css"));
         }
     }
