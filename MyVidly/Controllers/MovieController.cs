7b8a168004e620d568978bfa11a97dee3e37a8f7
Ankit <ankit@siemens.com>
2018-08-29 22:23:16 +0530

    Using ViewBag

--- MyVidly/Controllers/MovieController.cs
+++ MyVidly/Controllers/MovieController.cs
@@ -15,6 +15,17 @@ namespace MyVidly.Controllers
             return View(movie);
         }
 
+        //using ViewData
+        public ActionResult Random()
+        {
+            var movie = new Movie
+            {
+                Name = "Pirte"
+            };
+            ViewData["movie"] = movie;
+            return View();
+        }
+
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
