1528482ec7f807e85a4000cc27345f3cccdb4a7f
Ankit <ankit@siemens.com>
2018-09-11 23:21:13 +0530

    Changed navigation bar to have Customers and Movie hyperlink. Added new customer controller and related actions and view.

---
+++ MyVidly/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MyVidly.Models;
+
+namespace MyVidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().FirstOrDefault(x => x.Id == id);
+            return View(customer);
+        }
+
+        private List<Customer> GetCustomers()
+        {
+            return new List<Customer>{ new Customer {Id = 1,Name = "Raju"},new Customer{ Id = 5, Name = "Ramesh" } };
+        }
+    }
+}
