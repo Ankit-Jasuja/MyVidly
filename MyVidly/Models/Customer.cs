1627417be1c09367a5c6a6d2f474e3622261fd59
AD009\Z003T2SJ <ankit@siemens.com>
2018-09-30 19:59:48 +0530

    Added new column to customer table using entiy framework

--- MyVidly/Models/Customer.cs
+++ MyVidly/Models/Customer.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace MyVidly.Models
+﻿namespace MyVidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
     }
 }
