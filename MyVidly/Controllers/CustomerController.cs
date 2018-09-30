509b639eff3cfbe15628e1e519e73a014c54788b
AD009\Z003T2SJ <ankit@siemens.com>
2018-09-30 21:31:01 +0530

    understand defered execution and load customers from DB

--- MyVidly/Controllers/CustomerController.cs
+++ MyVidly/Controllers/CustomerController.cs
@@ -7,21 +7,31 @@ namespace MyVidly.Controllers
 {
     public class CustomerController : Controller
     {
+        public ApplicationDbContext Context { get; set; }
+        public CustomerController()
+        {
+            Context = new ApplicationDbContext();
+        }
+        
         // GET: Customer
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            // var customers = Context.Customers;//when this line is executed,entity framework is not going to query the data base,this is called defered execution
+            //the query is executed when we iterate over customers object
+            //this means query will not be executed immediately.
+
+            var customers = Context.Customers.ToList(); //query will be executed immediately
             return View(customers);
         }
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().FirstOrDefault(x => x.Id == id);
+            var customer = Context.Customers.FirstOrDefault(x => x.Id == id);
             return View(customer);
         }
 
         private List<Customer> GetCustomers()
         {
-            return new List<Customer>{ new Customer {Id = 1,Name = "Raju"},new Customer{ Id = 5, Name = "Ramesh" } };
+             return new List<Customer>{ new Customer {Id = 1,Name = "Raju"},new Customer{ Id = 5, Name = "Ramesh" } };
         }
     }
 }
