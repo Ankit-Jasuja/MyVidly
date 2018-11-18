3e9373f76723f62013c85c78083d85a4e7c50e2d
Ankit <ankit@siemens.com>
2018-11-18 15:26:28 +0530

    creating REST API for customers

---
+++ MyVidly/Api/CustomersController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using MyVidly.Models;
+
+namespace MyVidly.Api
+{
+    public class CustomersController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(z => z.Id == id);
+            if (customer == null)
+            {
+              throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return customer;
+        }
+
+        [HttpPost]
+        public Customer Create(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return customer;
+        }
+
+        [HttpPut]
+        public void UpdateCustomer(int id,Customer customer)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(z => z.Id == id);
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            customerInDb.Name = customer.Name;
+            customerInDb.BirthDate = customer.BirthDate;
+            customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            
+            _context.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(z => z.Id == id);
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+        }
+
+
+    }
+}
