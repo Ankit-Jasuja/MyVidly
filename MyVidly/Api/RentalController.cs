dd00053071d2b7843bab58438286eda79c3061fb
Ankit <ankit@siemens.com>
2019-01-06 21:06:08 +0530

    New Feature : Added skelton of API controller and new model to rent movies

---
+++ MyVidly/Api/RentalController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+using MyVidly.Models;
+
+namespace MyVidly.Api
+{
+    public class RentalController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalModel newRentalModel)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ MyVidly/Models/NewRentalModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MyVidly.Models
+{
+    public class NewRentalModel
+    {
+        public Movie Movie { get; set; }
+        public IEnumerable<Customer> Customers { get; set; }
+    }
+}
