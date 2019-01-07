051930375398e5b789560de3c48db0751fafe2fe
Ankit <ankit@siemens.com>
2019-01-07 19:58:18 +0530

    Removing the conditional statements as the API is going to be used by only our application,keeping only the valid checks

--- MyVidly/Api/RentalController.cs
+++ MyVidly/Api/RentalController.cs
@@ -16,24 +16,10 @@ namespace MyVidly.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalModel newRentalModel)
         {
-            if (!newRentalModel.MovieIdList.Any())
-            {
-                return BadRequest("Movie Id list is empty");
-            }
-
-            var customer = _applicationDbContext.Customers.ToList().SingleOrDefault(m => m.Id == newRentalModel.CustomerId);
-            if (customer == null)
-            {
-                return BadRequest("Customer id is Invalid");
-            }
+            var customer = _applicationDbContext.Customers.ToList().Single(m => m.Id == newRentalModel.CustomerId);
 
             var moviesInDb = _applicationDbContext.Movies.Where(z => newRentalModel.MovieIdList.Contains(z.Id))
                 .ToList();
-
-            if (moviesInDb.Count != newRentalModel.MovieIdList.Count())
-            {
-                return BadRequest("one or more movie id is invalid");
-            }
 
             foreach (var movie in moviesInDb)
             {
