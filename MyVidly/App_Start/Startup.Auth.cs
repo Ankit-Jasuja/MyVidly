10a73120e2600a78e079d067c4543c5985300841
Ankit <ankit@siemens.com>
2019-01-18 22:19:22 +0530

    Move Facebook settings to web.config and used an example to understand how key value can be made different in different web configations

--- MyVidly/App_Start/Startup.Auth.cs
+++ MyVidly/App_Start/Startup.Auth.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
@@ -55,8 +56,9 @@ namespace MyVidly
             //   consumerSecret: "");
 
             app.UseFacebookAuthentication(
-               appId: "227028874905704",
-               appSecret: "d5f9b472fd6c59e26a458284bca9d4c0");
+                appId: ConfigurationManager.AppSettings["FacebookAppId"],
+                appSecret: ConfigurationManager.AppSettings["FacebookAppSecret"]
+            );
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
